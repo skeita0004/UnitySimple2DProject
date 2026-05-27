ead425a76cb80972861ed8e9037f36f966004e47
Keita Saito <saitou-keita0109@outlook.jp>
2026-05-27 12:49:47 +0900

    タイトルシーンを改善

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,13 +1,21 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    [SerializeField] private string _scene;
+    [SerializeField] public string scene_;
+
+    private float timer_ = 0f;
+    private const float SCENE_CHANGE_TIME = 1.5f;
+
+    public static bool isSceneChage = false;
+
     public void ChangeScene()
     {
-        SceneManager.LoadScene(_scene);
+        SceneManager.LoadScene(scene_);
     }
 
     void Start()
@@ -17,9 +25,24 @@ public class SceneChange : MonoBehaviour
 
     void Update()
     {
-        if ( Keyboard.current.anyKey.isPressed )
+
+        if (isSceneChage)
         {
-            ChangeScene();
+            timer_ += Time.deltaTime;
+
+            // シーン切り替え時、1.5秒待機(演出のため)
+            if (timer_ > SCENE_CHANGE_TIME)
+            {
+                ChangeScene();
+            }
+        }
+        else
+        {
+            if ( Keyboard.current.anyKey.isPressed ||
+                Gamepad.current != null && Gamepad.current.allControls.Any(controller => controller is ButtonControl button && button.IsPressed()))
+            {
+                isSceneChage = true;
+            }
         }
     }
 
