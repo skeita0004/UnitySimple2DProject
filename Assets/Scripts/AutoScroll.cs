e48228ae342fbfb65864b6928e7058b34fa1cda2
Keita Saito <saitou-keita0109@outlook.jp>
2026-06-10 14:16:14 +0900

    プレハブ化されていないオブジェクトをプレハブ化

--- Assets/Scripts/AutoScroll.cs
+++ Assets/Scripts/AutoScroll.cs
@@ -5,13 +5,26 @@ public class AutoScroll : MonoBehaviour
     [Header("スクロール速度")]
     public float scrollSpeed = 2.0f;
 
+    [Header("プレイヤーオブジェクト")]
+    public GameObject player;
+
+    private bool scrollEnabled_ = false;
     void Update()
     {
-        // playerが、groundより上にいる時、かつ、playerが、最初の画面の右端に到達したら、スクロールスタート
-        if ( true /**/)
+        Vector2 playerPos = player.transform.position;
+
+        // playerが、groundより上にいる時、かつ、playerが最初の画面の右端に到達したら、スクロールスタート
+        scrollEnabled_ = (playerPos.y >= -3.5f) && (playerPos.x >= 5.5f);
+
+        if (scrollEnabled_)
         {
             // 毎フレーム、右方向（X軸プラス方向）へ一定速度で移動させる
             transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);
+
+            if (playerPos.y < -3.5f)
+            {
+                scrollEnabled_ = false;
+            }
         }
     }
 }
